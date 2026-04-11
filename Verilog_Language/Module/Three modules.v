57a92ed722a1ee7c17c943ba53ac17b0abb41ed1
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-11 10:32:37 +0530

    Create Three modules.v

---
+++ Verilog_Language/Module/Three modules.v
@@ -0,0 +1,6 @@
+module top_module ( input clk, input d, output q );
+    wire w1,w2;
+    my_dff m1(.clk(clk),.d(d),.q(w1));
+    my_dff m2(.clk(clk),.d(w1),.q(w2));
+    my_dff m3(.clk(clk),.d(w2),.q(q));
+endmodule
