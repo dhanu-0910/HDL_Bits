b2ab9fd26c77195b822f072fbad4b1c60d3a0e2a
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-13 12:07:37 +0530

    Create Thermostat.v

---
+++ Circuits/Combinational Logic/Basic Gates/Thermostat.v
@@ -0,0 +1,5 @@
+module top_module (input too_cold,input too_hot,input mode,input fan_on,output heater,output aircon,output fan); 
+    assign heater=too_cold & mode & ~aircon ;
+    assign aircon=~heater & ~mode & too_hot;
+    assign fan=fan_on | heater | aircon;
+endmodule
