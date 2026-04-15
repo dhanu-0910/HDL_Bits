b732fdc18b4ba64a4c116d29fc39907f299684b0
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-15 11:24:30 +0530

    Create Mux and DFF.v

---
+++ Circuits/Sequential Logic/Latches and Flip flop/Mux and DFF.v
@@ -0,0 +1,8 @@
+module top_module (input clk,input L,input r_in,input q_in,output reg Q);
+    always @(posedge clk) begin
+        case(L)
+            0: Q<=q_in;
+            1: Q<=r_in;
+        endcase
+    end
+endmodule
