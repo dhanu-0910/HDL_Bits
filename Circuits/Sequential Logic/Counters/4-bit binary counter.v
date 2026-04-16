571287b05815c1f514646c75eaa49e94b5fb3e3c
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-16 12:21:36 +0530

    Create 4-bit binary counter.v

---
+++ Circuits/Sequential Logic/Counters/4-bit binary counter.v
@@ -0,0 +1,9 @@
+module top_module (input clk,input reset,output [3:0] q);
+    always @(posedge clk) begin
+        if(reset)
+            q<=0;
+        else begin
+            q<=q+1;
+        end
+    end            
+endmodule
