a0f194ad4dfc3795a38cb89c02cc25a0a201bc0f
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-19 14:30:35 +0530

    Create Simple fsm 3 async reset.v

---
+++ Circuits/Sequential Logic/FSM/Simple fsm 3 async reset.v
@@ -0,0 +1,39 @@
+module top_module(input clk,input in,input areset,output out); 
+	parameter A=0, B=1, C=2, D=3;
+    reg [1:0] state,next_state;
+    always @(posedge clk or posedge areset) begin
+        if(areset)
+            state<=A;
+        else
+            state<=next_state;
+    end
+    always @(*) begin
+        case(state)
+            A:begin
+                if(in)
+                    next_state<=B;
+                else
+                    next_state<=A;
+            end
+            B:begin
+                if(in)
+                    next_state<=B;
+                else
+                    next_state<=C;
+            end
+            C:begin
+                if(in)
+                    next_state<=D;
+                else
+                    next_state<=A;
+            end
+            D:begin
+                if(in)
+                    next_state<=B;
+                else
+                    next_state<=C;
+            end
+        endcase
+    end
+    assign out=(state==D);
+endmodule
