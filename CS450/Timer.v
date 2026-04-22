187d8ab5803d5c3a4e2075b2207c5da287d7be92
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-22 18:16:18 +0530

    Update Timer.v

--- CS450/Timer.v
+++ CS450/Timer.v
@@ -1,11 +1,32 @@
-module top_module (input clk,input reset,output [9:0] q);
+module top_module(input clk, input load, input [9:0] data, output tc);
+    reg [9:0]counter;
     always @(posedge clk)begin
-        if(reset)begin
-            q<=0;
+        if(load) begin
+            counter<=data;
+            tc<=(data==10'd0)?1'b1:1'b0;
         end
-        else if(q>=999)
-            q<=0;
+        else begin
+            if(counter>10'b1)begin
+                counter<=counter-1'b1;
+                tc<=1'b0;
+            end
+            else begin
+                counter<=10'd0;
+                tc<=1'b1;
+            end                          
+        end
+    end       
+endmodule
+//Method 2
+module top_module (input clk,input load,input [9:0] data,output tc);
+    reg [9:0] count;
+    always @(posedge clk) begin
+        if (load)
+            count <= data;          
+        else if (count != 0)
+            count <= count - 1;     
         else
-            q=q+1;
+            count <= count;        
     end
+    assign tc = (count == 0);      
 endmodule
