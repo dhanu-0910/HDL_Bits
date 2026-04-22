2dbc0eba84c4404f746bc12f1200c31912be09b0
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-22 18:13:16 +0530

    Create Counter with period 1000.v

---
+++ CS450/Counter with period 1000.v
@@ -0,0 +1,11 @@
+module top_module (input clk,input reset,output [9:0] q);
+    always @(posedge clk)begin
+        if(reset)begin
+            q<=0;
+        end
+        else if(q>=999)
+            q<=0;
+        else
+            q=q+1;
+    end
+endmodule
