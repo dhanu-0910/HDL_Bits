c0899a6b692514a6726210ccd9d029578ffdcf71
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-22 18:11:38 +0530

    Create Testbench2.v

---
+++ Verification: Writing Testbench/Testbench2.v
@@ -0,0 +1,18 @@
+module top_module();    
+    reg clk;
+    reg in;
+    reg [2:0] s;
+    wire out;    
+    q7 dut(.clk(clk),.in(in),.s(s),.out(out));    
+    initial begin
+        clk = 0;
+        in = 0;s = 2;#10;
+        s = 6;#10;
+        s = 2;in = 1;#10;#10;
+        in = 1;s = 0;#30;
+        in = 0;
+    end    
+    always begin
+        #5 clk = ~clk;
+    end
+endmodule
