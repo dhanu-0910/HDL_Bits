ee9edd7ce3f094099261cc9c3266fad5e7d71f94
dhanu-0910 <dhanushiya952@gmail.com>
2026-04-27 16:14:48 +0530

    Create 6. FSM.v

---
+++ Circuits/Sequential Logic/FSM/6. FSM.v
@@ -0,0 +1,22 @@
+module top_module (input clk,input reset,input w,output z);
+	parameter a=0,b=1,c=2,d=3,e=4,f=5;
+    reg [3:0]state,next_state;
+    always @(posedge clk)begin
+        if(reset)
+            state<=a;
+        else
+            state<=next_state;
+    end
+    always @(*)begin
+        case(state)
+            a: next_state= w?a:b;
+            b: next_state= w?d:c;
+            c: next_state= w?d:e;
+            d: next_state= w?a:f;
+            e: next_state= w?d:e;
+            f: next_state= w?d:c;
+            default: next_state= a;
+        endcase
+    end
+    assign z=(state==e || state==f);            
+endmodule
